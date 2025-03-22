44d22de8ae1eca4fb34ce544bbd7d1f4165b5dba
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-03-22 14:19:12 +0900

    feat: env設定

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -1,11 +1,22 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 const configs = tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  {
+    name: 'base-setting',
+    files: ['**/*'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
 );
 
 export default configs;
