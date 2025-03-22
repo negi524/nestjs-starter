fdbf907f282c4ef15aa05ddc07f068286c3c3271
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-03-22 14:23:50 +0900

    feat: 残り全部移植

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -1,12 +1,14 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 const configs = tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  eslintConfigPrettier,
   {
     name: 'base-setting',
     files: ['**/*'],
@@ -15,6 +17,12 @@ const configs = tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 );
