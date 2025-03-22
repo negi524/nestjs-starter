bf73270e8c0d4094acb9f022ada6aec272c69ff5
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-03-22 14:30:52 +0900

    ESLintバージョンアップ (#2)
    * feat: upgrade yarn
    
    * feat: v9のconfigファイル作成
    
    * feat: env設定
    
    * feat: 残り全部移植
    
    * feat: yarn add -D eslint-config-prettier

---
+++ api/eslint.config.mjs
@@ -0,0 +1,30 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+const configs = tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  eslintConfigPrettier,
+  {
+    name: 'base-setting',
+    files: ['**/*'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+);
+
+export default configs;
