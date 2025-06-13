18331068f0c9040c4264a415fd0bbec2a9abbaef
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-06-13 18:56:43 +0900

    refactor: フォーマット指摘修正

--- api/src/health/health.controller.ts
+++ api/src/health/health.controller.ts
@@ -1,10 +1,9 @@
-import { Controller, Get, } from '@nestjs/common';
-import { ApiOperation, } from '@nestjs/swagger';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { HealthCheck, HealthCheckResult } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
-
   @Get()
   @HealthCheck()
   @ApiOperation({ summary: 'ヘルスチェックエンドポイント' })
