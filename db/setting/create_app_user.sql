3124aac8fa388089dc1056822d251cc5a4783155
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-07-06 17:59:29 +0900

    feat: アプリケーションロールにCURD権限付与

--- db/setting/create_app_user.sql
+++ db/setting/create_app_user.sql
@@ -1,3 +1,9 @@
 -- アプリケーション用のロールを新規作成する
-CREATE ROLE app_user LOGIN PASSWORD 'Password!'
+CREATE ROLE app_user LOGIN PASSWORD 'Password!';
+
 -- 作成したロールにテーブル操作の権限を付与する
+GRANT INSERT,
+SELECT
+,
+UPDATE,
+DELETE ON ALL TABLES in SCHEMA public TO app_user;
