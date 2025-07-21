8067cdbfe4bbd4b51124adb9d143d607e3b75689
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-07-21 16:55:31 +0900

    fix: ローカル環境でのDBユーザー運用方法定義

--- db/setting/create_app_user.sql
+++ db/setting/create_app_user.sql
@@ -1,9 +1,12 @@
--- アプリケーション用のロールを新規作成する
-CREATE ROLE app_user LOGIN PASSWORD 'Password!';
+-- PostgreSQL ユーザー初期化スクリプト
+-- アプリケーション用ユーザーを作成し、適切な権限を付与する
 
--- 作成したロールにテーブル操作の権限を付与する
-GRANT INSERT,
-SELECT
-,
-UPDATE,
-DELETE ON ALL TABLES in SCHEMA public TO app_user;
+-- アプリケーション用ユーザーを作成
+-- 環境変数 APP_USER_PASSWORD からパスワードを取得
+\set app_password `echo $APP_USER_PASSWORD`
+CREATE ROLE app_user LOGIN PASSWORD :'app_password';
+
+-- publicスキーマに対して、テーブルの作成権限を付与(マイグレーションで必要なため)
+-- 本来は付与すべきではないが、開発環境で`prisma migrate reset`を効率的に行うためにこのようにしている
+-- 本番環境ではアプリケーションユーザーに対してテーブルの作成と削除権限は付与すべきではない
+GRANT CREATE ON SCHEMA public TO app_user;
