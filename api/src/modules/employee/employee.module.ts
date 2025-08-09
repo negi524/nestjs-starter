2bd8245717c6cbaffb8986cf4bdc1eef263e8277
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-08-09 10:40:38 +0900

    refactor: employeeにバージョニング

--- api/src/modules/employee/employee.module.ts
+++ api/src/modules/employee/employee.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { EmployeeController } from './presentation/employee.controller';
+import { EmployeeV1Controller } from './presentation/employee.v1.controller';
 import { EmployeeUseCase } from './application/usecase/employee.usecase';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [EmployeeController],
-  providers: [EmployeeUseCase, EmployeeController],
+  controllers: [EmployeeV1Controller],
+  providers: [EmployeeUseCase, EmployeeV1Controller],
 })
 export class EmployeeModule {}
--- api/src/modules/employee/presentation/dto/request/employee-request.v1.dto.ts
+++ api/src/modules/employee/presentation/dto/request/employee-request.v1.dto.ts
@@ -3,7 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 /**
  * 従業員一覧リクエスト時のリクエストパラメータ
  */
-export default class EmployeeRequestDto {
+export class EmployeeRequestV1Dto {
   @ApiProperty({
     description: '従業員名',
     example: '山田',
