fb7450cb780d6308eb9a3b494c101e4ae68bc233
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-09-13 16:30:17 +0900

    fix: 環境変数をconfigServiceから取得するように変更

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,14 +1,19 @@
 import { Injectable, OnApplicationShutdown } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService implements OnApplicationShutdown {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
   onApplicationShutdown(signal?: string) {
     console.log(`onApplicationShutdown: ${signal}`);
   }
 
   getHello(): string {
-    return `Hello World! env=${process.env.NODE_ENV}`;
+    const envName = this.configService.get<string>('NODE_ENV');
+    return `Hello World! env=${envName}`;
   }
 }
