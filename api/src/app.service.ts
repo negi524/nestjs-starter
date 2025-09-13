e4888de2817ec0b77409cd4599cf852edb62c249
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-09-13 17:06:13 +0900

    feat: 環境変数の型定義実装

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, OnApplicationShutdown } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from './env.validaton';
 
 @Injectable()
 export class AppService implements OnApplicationShutdown {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService<EnvironmentVariables>,
+  ) {}
   onApplicationShutdown(signal?: string) {
     console.log(`onApplicationShutdown: ${signal}`);
   }
 
   getHello(): string {
-    const envName = this.configService.get<string>('NODE_ENV');
+    const envName = this.configService.get('NODE_ENV', { infer: true });
     return `Hello World! env=${envName}`;
   }
 }
